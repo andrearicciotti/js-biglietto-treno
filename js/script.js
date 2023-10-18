331c0bd904118c295cc5cce8fe65dc829efca170
andrearicciotti <ricciotti.andrea1993@gmail.com>
2023-10-18 17:02:17 +0200

    add: variables for input datas

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
+// RACCOLTA DATI
+const kmTrip = parseInt(prompt('Ciao! Quanti kilometri devi percorrere?'));
+console.log(kmTrip);;
+
+const eta = parseInt(prompt('E quanti anni hai?'));
+console.log(kmTrip);;
+
+const ticketPrice = kmTrip * 0.21;
+console.log(ticketPrice);
+
+const disc20 = 0.20
+
+const disc40 = 0.40
+
+console.log(disc20, disc40);
 
 
+// LOGICA PROGRAMMA
