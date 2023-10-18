fb2fb1986dc88bd8f424cc67af7b0c6f10a2cc99
andrearicciotti <ricciotti.andrea1993@gmail.com>
2023-10-18 18:03:01 +0200

    add: output message into html document

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 // RACCOLTA DATI
 const kmTrip = parseInt(prompt('Ciao! Quanti kilometri devi percorrere?'));
-console.log(kmTrip);
+// console.log(kmTrip);
 
 const eta = parseInt(prompt('E quanti anni hai?'));
-console.log(kmTrip);
+// console.log(eta);
 
 let ticketPrice = (kmTrip * 0.21).toFixed(2);
-console.log(ticketPrice);
+// console.log(ticketPrice);
 
 const discounts = [0.20 , 0.40];
 
@@ -14,7 +14,7 @@ let disc20 = discounts.at();
 
 let disc40 = discounts.at(1);
 
-console.log(disc20, disc40);
+// console.log(disc20, disc40);
 
 
 // LOGICA PROGRAMMA
@@ -23,4 +23,13 @@ if (eta < 18) {
 } else if (eta > 65) {
     ticketPrice = (ticketPrice - (ticketPrice * disc40)).toFixed(2);
 }
-console.log(ticketPrice);
+// console.log(ticketPrice);
+
+
+// OUTPUT MESSAGGIO
+const finalPrice = `<div id="ticket-price" class="col-12 text-center my-4 fs-4">
+<span>Your ticket price is <strong>${ticketPrice}$</span></div><div id="ticket-price" class="col-12 text-center my-4 fs-4">
+<span>Enjoy your trip! :)</span>
+</div>`
+
+document.getElementById('ticket-price').innerHTML = finalPrice;
