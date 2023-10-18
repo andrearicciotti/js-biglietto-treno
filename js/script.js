3ed68c0a5e64f58e3a956ad983c2c3a30521766c
andrearicciotti <ricciotti.andrea1993@gmail.com>
2023-10-18 17:38:08 +0200

    add: program logic

--- js/script.js
+++ js/script.js
@@ -1,18 +1,26 @@
 // RACCOLTA DATI
 const kmTrip = parseInt(prompt('Ciao! Quanti kilometri devi percorrere?'));
-console.log(kmTrip);;
+console.log(kmTrip);
 
 const eta = parseInt(prompt('E quanti anni hai?'));
-console.log(kmTrip);;
+console.log(kmTrip);
 
-const ticketPrice = kmTrip * 0.21;
+let ticketPrice = (kmTrip * 0.21).toFixed(2);
 console.log(ticketPrice);
 
-const disc20 = 0.20
+const discounts = [0.20 , 0.40];
 
-const disc40 = 0.40
+let disc20 = discounts.at();
+
+let disc40 = discounts.at(1);
 
 console.log(disc20, disc40);
 
 
 // LOGICA PROGRAMMA
+if (eta < 18) {
+    ticketPrice = (ticketPrice - (ticketPrice * disc20)).toFixed(2);
+} else if (eta > 65) {
+    ticketPrice = (ticketPrice - (ticketPrice * disc40)).toFixed(2);
+}
+console.log(ticketPrice);
